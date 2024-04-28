3499b99bdbcf0e1098057179e484c9a3db5e7890
darkgithub1 <darkgithub16457@gmail.com>
2024-04-28 17:42:05 +0530

    complete the post card feed section

--- src/components/MiniProfile.jsx
+++ src/components/MiniProfile.jsx
@@ -6,15 +6,15 @@ import { useSession, signIn, signOut } from 'next-auth/react';
 export default function MiniProfile() {
     const { data: session } = useSession();
     return (
-        <div className='flex items-center justify-between mt-14 scroll-ml-10'>
+        <div className='flex items-center justify-between mt-14 ml-10 w-full'>
         <img
-          src={session?.user?.image || '/codeteam.png'}
+          src={session?.user?.image || '/team.png'}
           alt='user-profile-pic or instagram logo'
           className='w-16 h-16 rounded-full border p-[2px]'
         />
         <div className='flex-1 ml-4'>
           <h2 className='font-bold'>{session?.user?.username}</h2>
-          <h3 className='text-sm text-gray-400'>Welcome to Instagram</h3>
+          <h3 className='text-sm text-gray-400'>Welcome to Code Team Share New Code Blocks</h3>
         </div>
         {session ? (
           <button
